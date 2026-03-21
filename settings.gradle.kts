49048aaae0fc5cf97ca1cc92b9c8551af59a0f08
manepough <manepough@gmail.com>
2026-03-21 15:09:26 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://repo.opencollab.dev/maven-snapshots/") }
         maven { url = uri("https://repo.opencollab.dev/maven-releases/") }
+        maven("https://repo.opencollab.dev/maven-snapshots/")
     }
 }
 rootProject.name = "Manes"
